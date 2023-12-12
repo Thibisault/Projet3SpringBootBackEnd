697fbc531635332547c72c8a5f7571f6f7cff5ef
Thibisault <72867272+Thibisault@users.noreply.github.com>
2023-12-12 16:11:37 +0100

    first commit
    first commit

---
+++ src/main/java/com/example/demo/Projet3BackEndApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Projet3BackEndApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Projet3BackEndApplication.class, args);
+	}
+
+}
